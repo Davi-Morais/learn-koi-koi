1a720fb1bd53222af88648e4fa93d54b41cc85da
Davi Morais <davifarias2433@gmail.com>
2023-09-05 08:19:08 -0300

    Atualizando nome do diretorio

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Regras from './pages/Regras';
+import Home from './componentes/Home';
+import Regras from './componentes/Regras';
 
 
 function AppRoutes() {
