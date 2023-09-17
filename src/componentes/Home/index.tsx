a60be9b36963f26d93d7cd793b6dcf5af4531bfa
Davi Morais <davifarias2433@gmail.com>
2023-09-17 15:07:15 -0300

    Ajustando estilo do site para os artigos

--- src/componentes/Home/index.tsx
+++ src/componentes/Home/index.tsx
@@ -6,27 +6,29 @@ function Home () {
 
     return (
         <>
-            <article>
-                <h1>Aprenda a jogar Koi Koi</h1>
+            <div className="blocoTexto">
+                <article className="artigo">
+                    <h1 className="titleArtigo">Aprenda a jogar Koi Koi</h1>
 
-                <p>Primeiramente, eu gostaria de informar que este site foi feito com apenas dois objetivos em mente:</p>
-                <ul>
-                <li>Desenvolver as minhas skills como programador.</li>
-                <li>Testar os meus conhecimentos sobre esse jogo de cartas que talvez você esteja interessado.</li>
-                </ul>
+                    <p>Primeiramente, eu gostaria de informar que este site foi feito com apenas dois objetivos em mente:</p>
+                    <ul>
+                    <li>Desenvolver as minhas skills como programador.</li>
+                    <li>Testar os meus conhecimentos sobre esse jogo de cartas que talvez você esteja interessado.</li>
+                    </ul>
 
-                <p>Sobre o primeiro ponto, bom, este é o meu primeiro projeto pessoal que eu realmente quero que vá para frente. </p>
+                    <p>Sobre o primeiro ponto, bom, este é o meu primeiro projeto pessoal que eu realmente quero que vá para frente. </p>
 
-                <p>Talvez ninguém na face da terra precise usar esse site para aprender a jogar Koi-Koi. Na verdade, o melhor jeito de aprender é jogando, claro. Mesmo assim, planejo dar continuidade a esse site sempre que tiver tempo livre, quero entregar para mim mesmo o melhor resultado possível, além de aprender com todos os erros e más escolhas que eu fizer por aqui.</p>
+                    <p>Talvez ninguém na face da terra precise usar esse site para aprender a jogar Koi-Koi. Na verdade, o melhor jeito de aprender é jogando, claro. Mesmo assim, planejo dar continuidade a esse site sempre que tiver tempo livre, quero entregar para mim mesmo o melhor resultado possível, além de aprender com todos os erros e más escolhas que eu fizer por aqui.</p>
 
-                <p>Sobre o segundo ponto, também não sou especialista em Koi-Koi, na verdade, comecei a jogar a pouco tempo(2023), em um app da Play Store. Não sei o nome de todas as cartas e combinações possíveis, muito menos das melhores estratégias (que eu vou ter que pesquisar muito daqui pra frente, lol), mas pretendo mudar essa realidade enquanto desenvolvo.</p>
+                    <p>Sobre o segundo ponto, também não sou especialista em Koi-Koi, na verdade, comecei a jogar a pouco tempo(2023), em um app da Play Store. Não sei o nome de todas as cartas e combinações possíveis, muito menos das melhores estratégias (que eu vou ter que pesquisar muito daqui pra frente, lol), mas pretendo mudar essa realidade enquanto desenvolvo.</p>
 
-                <p>A minha ideia inicial é construir uma página para cada tópico que eu achei mais importante para o jogo. Vou tentar colocar alguns quizes no final de cada página, como uma espécie de teste, para isto não ser uma experiência de somente leitura. Na verdade, os quizes são mais para me desafiar em conseguir fazê-los.</p>
+                    <p>A minha ideia inicial é construir uma página para cada tópico que eu achei mais importante para o jogo. Vou tentar colocar alguns quizes no final de cada página, como uma espécie de teste, para isto não ser uma experiência de somente leitura. Na verdade, os quizes são mais para me desafiar em conseguir fazê-los.</p>
 
-                <p>Por último, se você também for desenvolvedor, e não tiver absolutamente nada melhor para fazer, considere apoiar este projetinho dando sugestões de como seria uma melhor forma de fazer tal coisa, ou mesmo fazendo um pull request!</p>
+                    <p>Por último, se você também for desenvolvedor, e não tiver absolutamente nada melhor para fazer, considere apoiar este projetinho dando sugestões de como seria uma melhor forma de fazer tal coisa, ou mesmo fazendo um pull request!</p>
 
-                <p>Acho que é só isso por enquanto, "Stop"!</p>
-            </article>
+                    <p>Acho que é só isso por enquanto, "Stop"!</p>
+                </article>
+            </div>
 
             <Quiz quiz={dados} />
 
