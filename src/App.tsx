2fa019a3546d2facb1f1265da2150444016779e3
Davi Morais <davifarias2433@gmail.com>
2023-09-27 17:19:16 -0300

    Adicionando Aside em todo o app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import AppRoutes from "./routes"
 import Top from "./ui/componentes/Top"
+import Aside from "./ui/componentes/Aside"
 
 function App() {
 
   return (
     <>
       <Top />
+      <Aside />
       <AppRoutes />
     </>
   )
