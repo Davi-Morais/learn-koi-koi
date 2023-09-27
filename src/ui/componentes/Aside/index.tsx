d1d033e966fc415b63624ddccbc6f5a9c6ee5118
Davi Morais <davifarias2433@gmail.com>
2023-09-27 17:18:42 -0300

    Criando componente Aside

---
+++ src/ui/componentes/Aside/index.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+function Aside() {
+    return (
+        <>
+            <nav className="aside">
+                <ul className="rotas">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/regras">Regras</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Aside
